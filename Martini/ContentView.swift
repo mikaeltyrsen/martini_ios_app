8d210dd86f269ce1c57f16447c283f97767b984a
Mikael Tyrsen <mikael@nucontext.com>
2025-11-15 22:11:54 -0800

    first commit

---
+++ Martini/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Martini
+//
+//  Created by Mikael Tyrsen on 11/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Martini/MartiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  MartiniApp.swift
+//  Martini
+//
+//  Created by Mikael Tyrsen on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MartiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
