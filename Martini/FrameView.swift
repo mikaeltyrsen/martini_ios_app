530d480cb51285c9bead6ff1a4bef31f3c617522
Mikael Tyrsen <mikael@nucontext.com>
2026-01-16 11:36:55 -0800

    fixes

--- Martini/FrameView.swift
+++ Martini/FrameView.swift
@@ -560,8 +560,6 @@ struct FrameView: View {
             VStack(spacing: 4) {
                 Text(frameTitle)
                     .font(.headline)
-                Text("test")
-                    .font(.headline)
                 if let creativeTitleText {
                     creativeTitleCapsule(creativeTitleText)
                         .transition(.opacity.combined(with: .move(edge: .top)))
